a4a188c2edc0cf17ca6fdc7daa3c208477117f35
Adekoya Daniel <adekoyadaniel53@gmail.com>
2021-01-21 02:04:41 +0100

    routing config

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,21 @@ export const router = createRouter({
             path: '/',
             name: 'Landing',
             component: () => import('../views/Landing.vue')
+        },
+        {
+            path: '/signup',
+            name: 'Signup',
+            component: () => import('../views/CreateAccount.vue')
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: () => import('../views/Login.vue')
+        },
+        {
+            path: '/admin',
+            name: 'Admin',
+            component: () => import('../views/Admin.vue')
         }
     ],
     history: createWebHistory()
